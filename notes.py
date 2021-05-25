b19f068afe5d11ad42a7767e7af5c5c08fde56ab
强 <1677412161@qq.com>
2021-05-25 20:55:57 +0800

    Create notes.py

---
+++ notes.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+# 在python2.X中，像python3.X那样使用print()语法
+
+# python2.7
+print "Hello world"
+
+# python3
+print("Hello world")
+
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import with_statement # 等等
+
